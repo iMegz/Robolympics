384221fcdab048cdcc17727ddc9e612f189ea1ad
Ahmed Magdi <ammhaa2012@gmail.com>
2022-07-03 14:01:15 +0200

    Project initialization
    - Added constants in arduino code
    - Prepared functions headers

---
+++ Arduino/arduino.ino
@@ -0,0 +1,39 @@
+#include "constants.h"
+
+int speed = 0;
+
+void setup() {
+  
+}
+
+void loop() {
+  
+
+}
+
+//Movement
+void move(int dir){
+
+}
+
+void stop(){
+  
+}
+
+void changeSpeed(int s){
+  
+}
+
+//Arm
+void moveArm(int op){
+  
+}
+
+void fire(){
+  
+}
+
+//Sensors
+int distance(int dir){
+  
+}
---
+++ Arduino/constants.h
@@ -0,0 +1,54 @@
+//Directions
+#define FRONT 0
+#define BACK 1
+#define RIGHT 2
+#define LEFT 3
+#define ROTATE 4
+
+//Arm 
+#define UP 0
+#define DOWN 1
+#define OPEN 2
+#define CLOSE 3
+
+//Minimum distance before stopping
+#define MIN_DISTANCE 10
+
+//--------------------------------------//
+//---------------- PINS ----------------//
+//--------------------------------------//
+
+//Ultra sonic sensors pins
+#define FRONT_ECHO 0
+#define FRONT_TRIG 1 
+
+#define BACK_ECHO 0
+#define BACK_TRIG 1 
+
+#define RIGHT_ECHO 0
+#define RIGHT_TRIG 1
+
+#define LEFT_ECHO 0
+#define LEFT_TRIG 1
+
+//Motors pins, F = Front, R = Right, M = Motor ... (i.e. FRM = Front right motor)
+#define FRM_SPEED 0
+#define FRM_IN1 1 
+#define FRM_IN2 2
+
+#define FLM_SPEED 0
+#define FLM_IN1 1 
+#define FLM_IN2 2
+
+#define BRM_SPEED 0
+#define BRM_IN1 1 
+#define BRM_IN2 2
+
+#define BLM_SPEED 0
+#define BLM_IN1 1 
+#define BLM_IN2 2
+
+//Arm
+#define MOVE_SERVO 0 
+#define JAW_SERVO 1
+#define FIRE_MOTOR 2
