19043b939a4dbe28f5bfe0524fc52c019979a576
Ahmed Abdallah <85197116+ahmed1809252@users.noreply.github.com>
2022-07-05 04:34:09 +0200

    Add files via upload

---
+++ BlueTooth_init/Bluetooth/Bluetooth.ino
@@ -0,0 +1,34 @@
+// The initialization of bluetooth for ESP32
+
+#include "BluetoothSerial.h"
+
+// check if bluetooth is enabled or not 
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+  // if the bluetooth of device is available (connected with ESP32),
+  //then ESP32 bluetooth will read the msg from device and send it to ESP (to show it) vice versa
+  
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
